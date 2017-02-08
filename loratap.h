5c661fc0855bcb7906e0c34eba536e62c11931a5
Erik de Jong <eriknl@users.noreply.github.com>
2017-02-08 21:00:07 +0100

    Update loratap.h

--- loratap.h
+++ loratap.h
@@ -1,18 +1,18 @@
 /*
 This file is part of LoRaTap Copyright (C) 2017 Erik de Jong
 
-LoRaLog is free software: you can redistribute it and/or modify
+LoRaTap is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-LoRaLog is distributed in the hope that it will be useful,
+LoRaTap is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with LoRaLog.  If not, see <http://www.gnu.org/licenses/>.
+along with LoRaTap.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <stdint.h>
