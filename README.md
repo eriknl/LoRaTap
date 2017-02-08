8a0d65d211d9927ce22362012f2f67081aa9eb00
erikdejong <erikdejong@gmail.com>
2017-02-08 20:58:26 +0100

    Initial commit to publish header format

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # LoRaTap
 LoRaTap encapsulation
+
+Encapsulation format to be used to store LoRa traffic in pcap files.
+
+```
+typedef struct loratap_header {
+	uint16_t			lt_version;	/* LoRatap header version */
+	uint16_t			lt_len;		/* LoRatap header length */
+	uint8_t				packet_rssi;	/* LoRa packet RSSI */
+	uint8_t				rssi;		/* LoRa receiver RSSI */
+	uint8_t				snr;		/* LoRa SNR */
+	uint8_t				_padding;
+	uint32_t			frequency;	/* LoRa frequency */
+	uint8_t				channel;	/* LoRa channel */
+	uint8_t				sf;		/* LoRa SF */
+	uint16_t			length;		/* LoRa packet length */
+} loratap_header_t;
+```
---
+++ loratap.h
@@ -0,0 +1,31 @@
+/*
+This file is part of LoRaTap Copyright (C) 2017 Erik de Jong
+
+LoRaLog is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+LoRaLog is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with LoRaLog.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdint.h>
+
+typedef struct loratap_header {
+	uint16_t			lt_version;	/* LoRatap header version */
+	uint16_t			lt_len;		/* LoRatap header length */
+	uint8_t				packet_rssi;	/* LoRa packet RSSI */
+	uint8_t				rssi;		/* LoRa receiver RSSI */
+	uint8_t				snr;		/* LoRa SNR */
+	uint8_t				_padding;
+	uint32_t			frequency;	/* LoRa frequency */
+	uint8_t				channel;	/* LoRa channel */
+	uint8_t				sf;		/* LoRa SF */
+	uint16_t			length;		/* LoRa packet length */
+} loratap_header_t;
