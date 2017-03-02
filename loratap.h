cec875b40b0590fcaea38ac722f6e7cb50ae0b68
erikdejong <erikdejong@gmail.com>
2017-03-02 21:42:27 +0100

    Added containers for RSSI and channel Added syncword

--- loratap.h
+++ loratap.h
@@ -25,22 +25,31 @@ along with LoRaTap.  If not, see <http://www.gnu.org/licenses/>.
 	lt_padding	Unused, for boundary alignment.
 	lt_length	LoRaTap header length, field used to allow expansion in future versions. Each version can append header parameters. 
 			So, whilst parsing, these parameters can be skipped if not defined for older versions by skipping to the offset of lt_length starting from the beginning of the header.
-	packet_rssi	RSSI of last packet (dBm)
-	rssi		Current RSSI (dBm)
-	snr		Estimation of last packet SNR (dB)
-	sf		The spreading factor as used for the radio packet
-	frequency	Frequency packet was received on (MHz)
+	channel		Channel information
+	rssi		RSSI information
+	sync_word	Sync word for received packet, value 0x34 is reserved for LoRaWAN packets
 */
 
 typedef enum sf { SF7=7, SF8, SF9, SF10, SF11, SF12 } sf_t;
+
+typedef struct loratap_channel {
+	uint32_t			frequency;	/* LoRa frequency (Hz) */
+	uint8_t				bandwidth;	/* Channel bandwidth (KHz) in 125 KHz steps */
+	uint8_t				sf;		/* LoRa SF (sf_t) [7, 8, 9, 10, 11, 12] */
+} loratap_channel_t __attribute__ ((packed));
+
+typedef struct loratap_rssi {
+	uint8_t				packet_rssi;	/* LoRa packet RSSI, if snr >= 0 then dBm value is -139 + packet_rssi, otherwise dBm value is -139 + packet_rssi * .25 */
+	uint8_t				max_rssi;	/* LoRa receiver max RSSI (dBm value is -139 + rssi) */
+	uint8_t				current_rssi;	/* LoRa receiver current RSSI (dBm value is -139 + rssi) */
+	uint8_t				snr;		/* LoRa SNR (dB value is (snr[two's complement])/4) */
+} loratap_rssi_t __attribute__ ((packed));
 
 typedef struct loratap_header {
 	uint8_t				lt_version;	/* LoRatap header version */
 	uint8_t				lt_padding;
 	uint16_t			lt_length;	/* LoRatap header length */
-	uint8_t				packet_rssi;	/* LoRa packet RSSI (dBm) */
-	uint8_t				rssi;		/* LoRa receiver RSSI (dBm) */
-	uint8_t				snr;		/* LoRa SNR (dB) */
-	uint8_t				sf;		/* LoRa SF [sf_t] */
-	uint32_t			frequency;	/* LoRa frequency (MHz) */
+	loratap_channel_t		channel;
+	loratap_rssi_t			rssi;
+	uint8_t				sync_word;	/* LoRa radio sync word [0x34 = LoRaWAN] */
 } loratap_header_t __attribute__ ((packed));
