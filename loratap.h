76cf843fef4d74d181e71cc507d12a72831d942a
erikdejong <erikdejong@gmail.com>
2017-06-03 12:36:31 +0200

    Fixed packing attribute

--- loratap.h
+++ loratap.h
@@ -32,24 +32,24 @@ along with LoRaTap.  If not, see <http://www.gnu.org/licenses/>.
 
 typedef enum sf { SF7=7, SF8, SF9, SF10, SF11, SF12 } sf_t;
 
-typedef struct loratap_channel {
+typedef struct __attribute__((__packed__)) loratap_channel {
 	uint32_t			frequency;	/* LoRa frequency (Hz) */
 	uint8_t				bandwidth;	/* Channel bandwidth (KHz) in 125 KHz steps */
 	uint8_t				sf;		/* LoRa SF (sf_t) [7, 8, 9, 10, 11, 12] */
-} loratap_channel_t __attribute__ ((packed));
+} loratap_channel_t;
 
-typedef struct loratap_rssi {
+typedef struct __attribute__((__packed__)) loratap_rssi {
 	uint8_t				packet_rssi;	/* LoRa packet RSSI, if snr >= 0 then dBm value is -139 + packet_rssi, otherwise dBm value is -139 + packet_rssi * .25 */
 	uint8_t				max_rssi;	/* LoRa receiver max RSSI (dBm value is -139 + rssi) */
 	uint8_t				current_rssi;	/* LoRa receiver current RSSI (dBm value is -139 + rssi) */
 	uint8_t				snr;		/* LoRa SNR (dB value is (snr[two's complement])/4) */
-} loratap_rssi_t __attribute__ ((packed));
+} loratap_rssi_t;
 
-typedef struct loratap_header {
+typedef struct __attribute__((__packed__)) loratap_header {
 	uint8_t				lt_version;	/* LoRatap header version */
 	uint8_t				lt_padding;
 	uint16_t			lt_length;	/* LoRatap header length */
 	loratap_channel_t		channel;
 	loratap_rssi_t			rssi;
 	uint8_t				sync_word;	/* LoRa radio sync word [0x34 = LoRaWAN] */
-} loratap_header_t __attribute__ ((packed));
+} loratap_header_t;
